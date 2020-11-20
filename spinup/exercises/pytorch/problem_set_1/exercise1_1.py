fc4a124502d1d3d31d70e69a42476ee68aa80280
ChristopherMichael-Stokes <christopher.michael-stokes@city.ac.uk>
2020-11-20 12:47:13 +0000

    Complete exercise 0

--- spinup/exercises/pytorch/problem_set_1/exercise1_1.py
+++ spinup/exercises/pytorch/problem_set_1/exercise1_1.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import numpy as np
 
 """
@@ -13,6 +14,10 @@ likelihoods of those samples.
 
 """
 
+PI = torch.tensor(np.pi)
+DEBUG = True
+
+
 def gaussian_likelihood(x, mu, log_std):
     """
     Args:
@@ -23,12 +28,20 @@ def gaussian_likelihood(x, mu, log_std):
     Returns:
         Tensor with shape [batch]
     """
-    #######################
-    #                     #
-    #   YOUR CODE HERE    #
-    #                     #
-    #######################
-    return torch.zeros(1)
+    if DEBUG:
+        print(f'x: {x.shape},\nmu: {mu.shape}\nlog_std: {log_std.shape}')
+
+    num   = x - mu
+    # get standard deviation by exponentiating log_std
+    denom = torch.exp (log_std)
+    expr  = (num / denom)**2 + (2 * log_std)
+    expr  = expr + torch.log(2*PI)
+    expr  = -0.5 * expr.sum(axis=-1) 
+
+    if DEBUG:
+        print(f'output shape: {expr.shape}')
+
+    return expr
 
 
 if __name__ == '__main__':
@@ -38,18 +51,23 @@ if __name__ == '__main__':
     from spinup.exercises.pytorch.problem_set_1_solutions import exercise1_1_soln
     from spinup.exercises.common import print_result
 
-    batch_size = 32
-    dim = 10
+    batch_size = 32 
+    dim = 10 
 
-    x = torch.rand(batch_size, dim)
-    mu = torch.rand(batch_size, dim)
-    log_std = torch.rand(dim)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = torch.device('cuda')
+    print(f'using {device}')
+
+    PI = PI.to(device)
+    x = torch.rand(batch_size, dim, device=device)
+    mu = torch.rand_like(x)
+    log_std = torch.rand(dim, device=device)
 
     your_gaussian_likelihood = gaussian_likelihood(x, mu, log_std)
     true_gaussian_likelihood = exercise1_1_soln.gaussian_likelihood(x, mu, log_std)
 
-    your_result = your_gaussian_likelihood.detach().numpy()
-    true_result = true_gaussian_likelihood.detach().numpy()
+    your_result = your_gaussian_likelihood.cpu().detach().numpy()
+    true_result = true_gaussian_likelihood.cpu().detach().numpy()
 
     correct = np.allclose(your_result, true_result)
     print_result(correct)
